d2d2e3df1155acf9ec280840267d54cfb7db9ff7
Nguyen Truong <23653651.truong@student.iuh.edu.vn>
2026-07-07 09:05:21 +0700

    chore(project): khởi tạo cấu trúc dự án Spring Boot

---
+++ src/main/java/vantruong/iuh/QuanLiSanPhamApplication.java
@@ -0,0 +1,13 @@
+package vantruong.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLiSanPhamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLiSanPhamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vantruong/iuh/QuanLiSanPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package vantruong.iuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLiSanPhamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
